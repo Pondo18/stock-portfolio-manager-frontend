3bb4b7d3eb2a2ffed350cbccd286ff1e66f47951
Moritz Moser <moritz@baseofm.de>
2020-12-06 13:08:06 +0100

    Ergänze Code um Login Interface und erste Datenbank Kommunikationen

--- database.py
+++ database.py
@@ -1,13 +1,32 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="moritz",
-  password="secret"
+    host="localhost",
+    user="moritz",
+    password="secret",
+    database="finance"
 )
 
-mycursor = mydb.cursor()
 
-#mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+def get_hash():
+    token_file = open("token.txt", "r")
+    return token_file.read().rstrip()
 
-print("hello")
+
+def synchro():
+    mycursor = mydb.cursor()
+    sqlStatement = "Select username, credits from userdata where hashcode = %s"
+    val = (get_hash(),)
+    mycursor.execute(sqlStatement, val)
+    myresult = mycursor.fetchall()
+    mycursor.close()
+    return myresult
+
+
+def create_entry(hashcode, username):
+    mycursor = mydb.cursor()
+    sqlStatement = "insert into userdata (hashcode, username, credits) values (%s, %s, %s)"
+    val = (hashcode, username, 0)
+    mycursor.execute(sqlStatement, val)
+    mydb.commit()
+    mycursor.close()
--- start.py
+++ start.py
@@ -1,7 +1,34 @@
 import yfinance as yf
+import hashlib
+from pathlib import Path
+import database
 
 
-msft = yf.Ticker("SAP")
+# https://pypi.org/project/yfinance/
 
-opt = msft.option_chain('2020-12-18')
-print(opt)
+
+def login():
+    username = input("Please enter a username:\n")  # maximal 20 zeichen
+    password = input("Please enter a password:\n")  # Passwort Anforderungen hinzufügen
+    hash = create_hash(username + ":" + password)
+    database.create_entry(hash, username)
+
+
+def create_hash(password):
+    token_file = open("token.txt", "w")
+    hashcode = hashlib.md5(password.encode("utf-8")).hexdigest()
+    token_file.write(hashcode)
+    token_file.close()
+    return hashcode
+
+
+def start():
+    fname = Path("/Users/moritz.moser/Documents/HDBW/1.Semester/Python/Aktien/token.txt")
+    if (not fname.exists()):
+        login()
+    else:
+        database.synchro()
+
+
+if __name__ == "__main__":
+    start()
